e2085a54afd4a4ac0191fa8411d902f8b198ab91
Omar Aljazairy <onha2001@gmail.com>
2026-06-12 13:06:29 +0200

    adjusted local development routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './routers/AppRouter'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={basename || '/'}>
       <AppRouter />
     </BrowserRouter>
   )
